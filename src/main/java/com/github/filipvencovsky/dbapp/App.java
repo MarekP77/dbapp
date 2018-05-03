bd4d971676447bcdc769b923ebcebb1cc53229f8
filipvencovsky <filip.vencovsky@gmail.com>
2018-05-03 14:09:14 +0200

    vytvoření a načtení zaměstnance

--- src/main/java/com/github/filipvencovsky/dbapp/App.java
+++ src/main/java/com/github/filipvencovsky/dbapp/App.java
@@ -10,7 +10,16 @@ import org.hibernate.cfg.Configuration;
  *
  */
 public class App {
+
 	public static void main(String[] args) throws Exception {
+		
+		vytvorZamestnance();
+		
+		nactiZaměstnance(0);
+
+	}
+
+	private static void vytvorZamestnance() throws Exception {
 		// vytvoření objektu pro uložení do DB
 		Zamestnanec zam = new Zamestnanec();
 		zam.setId(0);
@@ -24,6 +33,9 @@ public class App {
 		// načtení konfiguračního souboru hibernate.cfg.xml, při defaultním jménu se
 		// nemusí nic uvádět
 		conf.configure();
+		
+		//chceme vytvořit tabulku a smazat pokud existovala
+		conf.setProperty("hibernate.hbm2ddl.auto", "create");
 
 		// načtení anotací z třídy
 		conf.addAnnotatedClass(Zamestnanec.class);
@@ -59,6 +71,58 @@ public class App {
 		// ukončení běžící instance, která vrací databázové relace - jinak program
 		// neskončí
 		factory.close();
+	}
+	
+	private static void nactiZaměstnance(int id) throws Exception {
+		// vytvoření prázdnéo objektu pro načtení zaměstnance z DB
+		Zamestnanec zam = new Zamestnanec();
 
+		// vytvoření prázdné konfigurace
+		Configuration conf = new Configuration();
+
+		// načtení konfiguračního souboru hibernate.cfg.xml, při defaultním jménu se
+		// nemusí nic uvádět
+		conf.configure();
+		
+		//neupravuje se schéma DB
+		conf.setProperty("hibernate.hbm2ddl.auto", "validate");
+
+		// načtení anotací z třídy
+		conf.addAnnotatedClass(Zamestnanec.class);
+
+		// získání odkazu na budoucí databázovou relaci, předá nám ji konfigurační
+		// objekt, který jsme předtím vytvořili
+		SessionFactory factory = conf.buildSessionFactory();
+
+		// otevření databázové relace
+		Session ses = factory.openSession();
+		Transaction tx = null;
+		try {
+
+			// otevření transakce
+			tx = ses.beginTransaction();
+
+			// uložení stavu objektu
+			ses.load(zam, id);
+			
+			System.out.println(zam.getPrijmeni()+", "+zam.getJmeno()+", "+zam.getPlat());
+
+			// odeslání transakce
+			tx.commit();
+			
+		} catch (Exception e) {
+			// rollback při problému
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		} finally {
+			// ukončení databázové relace po všech požadovaných operacích s DB, úklid pod
+			// připojení
+			ses.close();
+		}
+
+		// ukončení běžící instance, která vrací databázové relace - jinak program
+		// neskončí
+		factory.close();
 	}
 }
